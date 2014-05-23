70ce3a6ae6b1645281bf9798026d0fb1b0864c68
Chris Villalta <cfvillalta@gmail.com>
2014-05-23 11:22:31 -0700

    split lines and tested

--- make_boxplots_growth_rate_tyr_kds.py
+++ make_boxplots_growth_rate_tyr_kds.py
@@ -1,11 +1,16 @@
 #! /usr/bin/python
 
+import sys
+
 growth_data = sys.argv[1]
 
 fh = open(growth_data)
 
 data = fh.readlines()
 
+for line in data:
+    line = line.strip()
+    lines = line.split(",")
 
-print data
+    print lines
 
