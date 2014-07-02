953d1496acd668b28b7adfed46b77ed6bd2748b4
Chris Villalta <cfvillalta@gmail.com>
2014-07-02 16:34:51 -0700

    converted to module. called trim

--- import_abi_seqs_trim_ends.py
+++ import_abi_seqs_trim_ends.py
@@ -1,34 +1,34 @@
 #! /usr/bin/python
 
-
+def trim(input_directory_here)
 #will be a script where I input a batch of abi files and they are trimmed and out put as sequence files.x
 
-import sys
-import os
+    import sys
+    import os
 
-from Bio import SeqIO
+    from Bio import SeqIO
 
-seq_dir = sys.argv[1]
+    seq_dir = input_directory_here
 
-seqs = {}
+    seqs = {}
 
-for file in os.listdir(seq_dir):
-    if file.endswith(".ab1"):
-        file_path = "%s%s" %(seq_dir,file)
+    for file in os.listdir(seq_dir):
+        if file.endswith(".ab1"):
+            file_path = "%s%s" %(seq_dir,file)
         
-        handle = open(file_path, 'rb')
+            handle = open(file_path, 'rb')
 
 
 
-        for record in SeqIO.AbiIO.AbiIterator(handle, trim = True):
-            print record.id
-            print 
-            print record
-
-            seqs[record.id] = record
+            for record in SeqIO.AbiIO.AbiIterator(handle, trim = True):
+                #print record.id
+                #print 
+                #print record
+                
+                seqs[record.id] = record
     
             # output_handle = open("test.fasta", "w")
             # SeqIO.write(record, output_handle, "fasta")
             #output_handle.close()
 
-print seqs
+    return(seqs)
