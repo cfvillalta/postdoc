e51e739de081d46235dc90b398756f1e197f0dd7
Chris Villalta <cfvillalta@gmail.com>
2014-07-02 15:02:29 -0700

    import abi file, now cuts by quaility, and now puts the abi file into a dict grouped by ID name"

--- import_abi_seqs_trim_ends.py
+++ import_abi_seqs_trim_ends.py
@@ -4,19 +4,31 @@
 #will be a script where I input a batch of abi files and they are trimmed and out put as sequence files.x
 
 import sys
+import os
 
 from Bio import SeqIO
 
-test_seq = sys.argv[1]
+seq_dir = sys.argv[1]
 
-handle = open(test_seq, 'rb')
+seqs = {}
 
-for record in SeqIO.AbiIO.AbiIterator(handle, trim = True):
-    print record.seq
-    print record
-
-    output_handle = open("test.fasta", "w")
-    SeqIO.write(record, output_handle, "fasta")
-    output_handle.close()
+for file in os.listdir(seq_dir):
+    if file.endswith(".ab1"):
+        file_path = "%s%s" %(seq_dir,file)
+        
+        handle = open(file_path, 'rb')
 
 
+
+        for record in SeqIO.AbiIO.AbiIterator(handle, trim = True):
+            print record.id
+            print 
+            print record
+
+            seqs[record.id] = record
+    
+            # output_handle = open("test.fasta", "w")
+            # SeqIO.write(record, output_handle, "fasta")
+            #output_handle.close()
+
+print seqs
