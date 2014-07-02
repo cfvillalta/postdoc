163d00795c1875f3c1da519b6445d6183e16f40e
Chris Villalta <cfvillalta@gmail.com>
2014-07-02 14:36:23 -0700

    import abi file, now cuts by quaility, and outputs a trimmed fasta file.

--- import_abi_seqs_trim_ends.py
+++ import_abi_seqs_trim_ends.py
@@ -12,12 +12,11 @@ test_seq = sys.argv[1]
 handle = open(test_seq, 'rb')
 print handle
 
-for record in SeqIO.parse(handle, "abi"):
+for record in SeqIO.AbiIO.AbiIterator(handle, trim = True):
     print record.seq
-    
 
-   output_handle = open("test.fasta", "w")
-   SeqIO.write(record, output_handle, "fasta")
-   output_handle.close()
+    output_handle = open("test.fasta", "w")
+    SeqIO.write(record, output_handle, "fasta")
+    output_handle.close()
 
 
