71d1a694c31644ba1ef96bcd4281cf470d870e77
Chris Villalta <cfvillalta@gmail.com>
2014-07-02 14:14:43 -0700

    Can now import ABI sequence successfully.

--- import_abi_seqs_trim_ends.py
+++ import_abi_seqs_trim_ends.py
@@ -1,7 +1,19 @@
-
+#! /usr/bin/python
 
 
 #will be a script where I input a batch of abi files and they are trimmed and out put as sequence files.x
+
+import sys
+
+from Bio import SeqIO
+
+test_seq = sys.argv[1]
+
+handle = open(test_seq, 'rb')
+print handle
+
+for record in SeqIO.parse(handle, "abi"):
+   print record
 
 
 
