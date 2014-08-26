3d92db34640944014ca33e3d8887b75a92ed728b
Chris VIllalta <cfvillalta@gmail.com>
2014-08-26 15:45:40 -0700

    convert_stockholm_fasta converted to convert fasta aln to clustal aln.

---
+++ convert_fasta_clustal.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python
+
+
+import sys
+
+#input a fasta formatted alignment.
+fasta_file = sys.argv[1]
+
+file_name_split = fasta_file.split(".")
+
+print file_name_split
+
+
+#import AlignIO from Bio python
+from Bio import AlignIO
+
+input_fasta = open(fasta_file, "rU")
+output_clustal = open("%s.aln" %(file_name_split[0]), "w")
+
+
+alignment = AlignIO.parse(input_fasta, "fasta")
+AlignIO.write(alignment, output_clustal, "clustal")
+
+output_fasta.close()
+input_stockholm.close()
