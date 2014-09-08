5aefd5dabb4f6540a6b80fb4e08f0160fdb316fe
Chris VIllalta <cfvillalta@gmail.com>
2014-09-08 16:55:57 -0700

    can get data and outputs only things with keyword pathogenic into a dict file and things with no data into a list.Need to rename dict file not really a dictionary, but from one

--- get_snpedia_from_23andme_data.py
+++ get_snpedia_from_23andme_data.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python 
 
 import sys
+import re
 from wikitools import *
 #import data from 23andme SNP file.
-
-snp_in = open('/Users/cfvillalta/genome.txt','r')
+genome_file = sys.argv[1]
+snp_in = open(genome_file,'r')
 snp_data = snp_in.readlines()
 
 snp_dict = {}
@@ -19,28 +20,38 @@ for snp in snp_data:
         
 #print snp_dict
         
-        
-        
+keyword =re.compile('pathogenic',re.IGNORECASE) 
+
 
 snps_no_snpedia = []
-snp_interest = {}
+snp_dict_text = open('snp_data_out.dict', 'w')
 for snp in snp_dict:
 
     site = wiki.Wiki("http://bots.snpedia.com/api.php")
 #print snp
 #print site
-
     
     pagehandle = page.Page(site,snp)
     try:
         snp_page = pagehandle.getWikiText()
-        snp_interest[snp] = snp_page
+        if keyword.search(snp_page):
+            snp_dict_text.write('SNP=%s\n'%(snp))
+            snp_dict_text.write('SNPEDIA_DATA=\n%s' %snp_page)
+            print 'SNPedia data available for %s\tputatively pathogenic' %(snp)
+        else:
+            print 'SNPedia data available for %s\t no pathogenic data' %(snp)
     except:
         snps_no_snpedia.append(snp)
+        print 'no SNPedia data present for %s' %(snp)
         
+snp_dict_text.close()
+print 'all done'
+
+snp_no_data = open('list_snps_no_snpedia_data.list','w')
+for snp in snps_no_snpedia:
+    snp_no_data.write("snp\n")
     
 #print snps_no_snpedia
-
+snp_no_data.close()
         
-snp_dict_text = open('snp_data_out.dict', 'w')
-snp_dict_text.write(snp_dict)
+
