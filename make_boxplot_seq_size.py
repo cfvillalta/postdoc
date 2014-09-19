08526fb6b8e5b2a6bfe61cdcc0d185db58a97173
Chris VIllalta <cfvillalta@gmail.com>
2014-09-19 16:43:42 -0700

    script can get each seq and its length.

--- make_boxplot_seq_size.py
+++ make_boxplot_seq_size.py
@@ -2,4 +2,31 @@
 
 #input files and get a size of the seq and make box plots from it also output the quartiles and median. So I know where to make cutoffs.
 
+import sys
+import re
 
+fasta_in = sys.argv[1]
+fasta = open(fasta_in, 'rU')
+seqs = fasta.readlines()
+
+seqs_dict = {}
+
+for line in seqs:
+    line = line.strip()
+    id = re.compile(r"(>)(\d+)") 
+    match = id.search(line)
+    if match:
+        seq_id = match.group(2)
+#        print seq_id
+        seqs_dict[match.group(2)]=[]
+ 
+    else:
+#        print line
+        seqs_dict[seq_id].append(line)
+
+
+print seqs_dict
+
+for seq in seqs_dict:
+    length = len(seqs_dict[seq][0])
+    print length
