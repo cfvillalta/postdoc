5fd9ea1a862dd0c1bdbf11d2924d34bfbcd471bf
Chris Villalta <cfvillalta@gmail.com>
2014-10-11 22:31:23 -0700

    have datain list to make graph

--- dotplot_conidia_isolation_kd_data.py
+++ dotplot_conidia_isolation_kd_data.py
@@ -40,105 +40,72 @@ for sample in data:
     if data[sample][2] in tyrs:
 
         if data[sample][3] in tyrs[data[sample][2]]:
-            print '%s if tyr present, and light or dark' %(data[sample])
+           # print '%s if tyr present, and light or dark' %(data[sample])
             tyrs[data[sample][2]][data[sample][3]].append(data[sample])
         else:
 
 
             tyrs[data[sample][2]][data[sample][3]]=[data[sample]]
     else:
-        print '%s if tyr not present yet' %(data[sample])
+       # print '%s if tyr not present yet' %(data[sample])
         
         tyrs[data[sample][2]]={}
         tyrs[data[sample][2]][data[sample][3]]=[data[sample]]
 
+
+#tyrs e.g. vector_light, vector_dark,tyr_light, tyr_dark
+data_to_plot = {}
+num_cond = 0
+for tyr in tyrs:
+    for cond in tyrs[tyr]:
+        num_cond = num_cond+1
+        data_to_plot["%s_%s" %(tyr, cond)]= []
 #print tyrs
-#going to make a dotplot                                                                                             
+#for tyr in tyrs:
+#    print tyr
+ #   for cond in tyrs[tyr]:
+ #       print cond
+  #      for sample in  tyrs[tyr][cond]:
+   #         print tyr
+    #        print cond
+     #       print sample
+                    
+#print sorted(data_to_plot.keys())
 
-import numpy as np
-import matplotlib as mpl
-import pylab
+dotplot_data=[]
+for name in sorted(data_to_plot.keys()):
+ #   print data_to_plot[name]
+    name_split = name.split("_")
+#    print name_split
+    tyr_match = re.compile(name_split[0])
+    cond_match = re.compile(name_split[1])
+    add_list = []
+#    microconidia
+    for tyr in tyrs:
+        for cond in tyrs[tyr]:
+            for sample in tyrs[tyr][cond]:
+                match = tyr_match.search(tyr)
+                match_2 = cond_match.search(cond)
+                
+                if match and match_2:
+                    #microconidia
+                    add_list.append(sample[4])
+    dotplot_data.append(add_list)
+    add_list=[]                    
+#macroconidia
+    for tyr in tyrs:
+        for cond in tyrs[tyr]:
+            for sample in tyrs[tyr][cond]:
+                match = tyr_match.search(tyr)
+                match_2 = cond_match.search(cond)
 
-#mpl.use('agg')                                                                                                    
+                if match and match_2:
+                    #macroconidia                                                
+                    add_list.append(sample[5])
+    
+    dotplot_data.append(add_list)
 
-import matplotlib.pyplot as plt
-
-dotplot_data = []
-
-#made a dictionary with the key as vector...tyr1...tyr2...                                                         
-tyrs = {}
-microconidia = {}
-macrocondia = {}
-
-for tyr in tyrs:
-    tyr_light = "%s_light" %(tyrs[tyr][2])
-    tyr_dark = "%s_dark" %(data[tyr][2])
-    tyrs[tyr_light]= ""
-    tyrs[tyr_dark]= ""
-    growth_by_tyr[tyr_light] = []
-    growth_by_tyr[tyr_dark] = []
-
-    #print tyrs                                                                                                    
-    #print data                                                                                                    
-'''
-#lengths grouped by tyrosinases                                                                                    
-for tyr in tyrs:
-    for sample in data:
-        # print sample                                                                                             
-        #print tyr                                                                                                 
-        if tyr == "%s_light" %(data[sample][2]):
-            #growth_by_tyr[tyr] = append [data[sample]]                                                            
-            growth_by_tyr[tyr].append(float(data[sample][3]))
-            #print sample                                                                                          
-        elif tyr == "%s_dark" %(data[sample][2]):
-            growth_by_tyr[tyr].append(float(data[sample][5]))
-'''
-tyr_sorted = []
-tyr_growth_sorted = []
-tyr_bioreps = []
-tyr_sorted.append("")
-
-#print growth_by_tyr                                                                                               
-
-import random
-num = 0
-for x in sorted(tyrs):
-    num = num+1
-    print num
-    tyr_sorted.append(x)
-   # tyr_growth_sorted.extend(growth_by_tyr[x])
-   # bioreps =len()
-   # a=num-.15
-   # b=num+.15
-   # x_axis = [random.uniform(a,b) for p in range(0, bioreps)]
-   # tyr_bioreps.extend(x_axis)
-#print tyr_bioreps
-#print tyr_sorted
-#print tyr_growth_sorted
+print dotplot_data
 
 
-
-fig = plt.figure(1, figsize=(9, 6))
-ax = fig.add_subplot(111)
-## Custom x-axis labels                                                                                            
-ax.set_xticklabels(tyr_sorted)
-
-ax.set_xticklabels(tyr_sorted, rotation =90)
-ax.set_ylabel('radial growth in cm')
-ax.set_xlabel('tyroinase knockdowns grown in light or dark')
-ax = plt.gca()
-#ax.set_autoscale_on(False)                                                                                        
-
-
-fig.savefig('fig1.png', bbox_inches='tight')
-plt.tight_layout()
-boxplot_name = "%s.png" %(file_name_split_2[0])
-
-
-
-plt.axis(ymin=0,ymax=10)
-plt.plot(tyr_bioreps, tyr_growth_sorted, 'ro')
-fig.savefig(boxplot_name)
-plt.show()
-#I want to count lengths of list in growth by tyr dict.        
 
