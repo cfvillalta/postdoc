24cacda25f9bb4057a43f059ab1ba0c0d1534182
Chris VIllalta <cfvillalta@gmail.com>
2014-10-13 14:10:36 -0700

    makes plot just need to add x and y axis labels.

--- dotplot_conidia_isolation_kd_data.py
+++ dotplot_conidia_isolation_kd_data.py
@@ -61,20 +61,24 @@ for tyr in tyrs:
         num_cond = num_cond+1
         data_to_plot["%s_%s" %(tyr, cond)]= []
 #print tyrs
-#for tyr in tyrs:
+condition_list = ['']
+for tyr in sorted(tyrs):
 #    print tyr
- #   for cond in tyrs[tyr]:
- #       print cond
+    for cond in tyrs[tyr]:
+#        print cond
+         condition_list.append('%s_%s_microcondiia' %(tyr,cond))
+         condition_list.append('%s_%s_macrocondiia' %(tyr,cond))
   #      for sample in  tyrs[tyr][cond]:
    #         print tyr
     #        print cond
      #       print sample
                     
 #print sorted(data_to_plot.keys())
-
+import numpy as np
 dotplot_data=[]
+#print data_to_plot
 for name in sorted(data_to_plot.keys()):
- #   print data_to_plot[name]
+#    print name
     name_split = name.split("_")
 #    print name_split
     tyr_match = re.compile(name_split[0])
@@ -83,36 +87,67 @@ for name in sorted(data_to_plot.keys()):
 #    microconidia
     for tyr in tyrs:
         for cond in tyrs[tyr]:
+ #           condition_list.append('%s_%s_microcondiia' %(tyr,cond))
             for sample in tyrs[tyr][cond]:
                 match = tyr_match.search(tyr)
                 match_2 = cond_match.search(cond)
                 
                 if match and match_2:
                     #microconidia
-                    add_list.append(sample[4])
+                    add_list.append(np.log10(float(sample[4])))
+                    
     dotplot_data.append(add_list)
     add_list=[]                    
+
 #macroconidia
     for tyr in tyrs:
         for cond in tyrs[tyr]:
+#            condition_list.append('%s_%s_microcondiia' %(tyr,cond))
             for sample in tyrs[tyr][cond]:
                 match = tyr_match.search(tyr)
                 match_2 = cond_match.search(cond)
 
                 if match and match_2:
                     #macroconidia                                                
-                    add_list.append(sample[5])
+                    add_list.append(np.log10(float(sample[5])))
+                    
     
     dotplot_data.append(add_list)
-import random
+#all of the growth rate data /y axis ordered vector_light micro vector_ligth_marco then vector dark micro.....
 print dotplot_data
-num_conds = len(dotplot_data)
-num_conds_list =  [random.uniform(a,b) for p in range(0, num_conds)]
-import numpy as np
+print condition_list
+
+
+
+##############
+#organzing data for graphing
+##############
+
+import random
+num = 0
+y_axis_points = []
+x_axis_points= []
+for x in dotplot_data:
+    num = num+1
+    print num
+    bioreps = len(x)
+    a=num-.15
+    b=num+.15
+    x_axis = [random.uniform(a,b) for p in range(0, bioreps)]
+    x_axis_points.extend(x_axis)
+    y_axis_points.extend(x)
+
+
 import matplotlib as mpl
+mpl.use('WXAgg')
 import pylab
 
-#mpl.use('agg')
-
 import matplotlib.pyplot as plt
-#plt.plot(
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax=plt.gca()
+ax.set_xticklabels(condition_list, rotation =90)
+plt.axis(ymin=0,ymax=10,xmin=0,xmax=num+1)
+plt.plot(x_axis_points, y_axis_points, 'ro')
+fig.tight_layout()
+plt.show()
