4a3e164a781ab63586dadc0e07b079f77536a347
Chris VIllalta <cfvillalta@gmail.com>
2014-10-24 16:57:30 -0700

    script takes in GID gets seqs and outputs an HMM from aligned seqs

---
+++ UID_in_HMM_out.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python 
+
+import phylo_tools
+import sys
+
+input = sys.argv[1]
+input_s = input.split(".")
+
+input_open =open(input, 'rU')
+
+GIDs= input_open.readlines()
+
+GID_list = []
+
+for GID in GIDs:
+    GID=GID.strip()
+    GID_s=GID.split("/")
+#    print GID_s[0]
+    GID_list.append(GID_s[0])
+
+fasta_seqs = phylo_tools.GID2seq(GID_list)
+
+#print fasta_seqs
+
+fasta = open('%s.fasta' %(input_s[0]),'w')
+for seq in fasta_seqs:
+    fasta.write('>%s\n%s' %(seq, fasta_seqs[seq][1]))
+    
+phylo_tools.ClustalO(input_s[0])
+
+phylo_tools.hmmbuild('%s_clustalo' %(input_s[0]))
