62b8dfcf5d252e081020a5189d142272ad4334a4
Chris VIllalta <cfvillalta@gmail.com>
2014-11-13 16:11:08 -0800

    runs list of snps against snp for snps with pathogenic keyword

--- input_snp_list_print_data_out.py
+++ input_snp_list_print_data_out.py
@@ -5,9 +5,37 @@ import re
 from wikitools import *
 
 snp_file = sys.argv[1]
+snp_file_s = snp_file.split(".")
 snp_in = open(snp_file, 'r')
 snp_data = snp_in.readlines()
 
+keyword =re.compile('pathogenic',re.IGNORECASE) 
 for snp in snp_data:
     snp = snp.strip()
-    print snp
+    #print snp
+    snp_dict_text = open('%s_data_out.txt' %(snp_file_s[0]), 'w')
+    site = wiki.Wiki("http://bots.snpedia.com/api.php")
+    pagehandle = page.Page(site,snp)
+    try:
+        snp_page = pagehandle.getWikiText()
+        if keyword.search(snp_page):
+            snp_dict_text.write('SNP=%s\n'%(snp))
+            snp_dict_text.write('SNPEDIA_DATA=\n%s' %snp_page)
+            print 'SNPedia data available for %s\tputatively pathogenic' %(snp)
+        else:
+#            print 'SNPedia data available for %s\t no pathogenic data' %(snp)
+            pass
+    except:
+        pass
+        #print 'no SNPedia data present for %s' %(snp)
+        
+        snp_dict_text.close()
+
+status_file = open(done_extracting_snpedia_data.txt, 'w')
+status_file.write('%s_data_out.txt' %(snp_file_s[0]))
+status_file.close()
+
+
+    
+#print snps_no_snpedia
+
