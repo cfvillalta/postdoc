5364d353d155172b70996ee74504a1e2ddcf9cab
Chris VIllalta <cfvillalta@gmail.com>
2014-11-18 11:40:09 -0800

    finished comments in this script

--- UID_in_HMM_out_domain.py
+++ UID_in_HMM_out_domain.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python 
+#The purpose of this script was to put in a list of IDs from a phylogenetic tree branch and pull their sequecnes from a list of sequecnes I might have. The script then aligns those sequences with Clustal Omega and builds an HMM with the alignment.
 
 import phylo_tools
 import sys
@@ -29,29 +30,41 @@ for GID in GIDs:
     GID_s=GID.split("/")
 #    print GID_s[0]
     GID_list.append(GID_s[0])
-
+#inputs my txt file with fasta sequences.
 input_2_open = open(input_2, 'rU')
-
+#read each line of the text file into a list called seqs.
 seqs = input_2_open.readlines()
-input_seqs ={} 
+#created an empty dict I will put seqs into with the seq id as the key and the value being the seqeucne.
+input_seqs ={}
+#loop through list seq. 
 for seq in seqs:
+#strip each string in list of trailign whitespace e.g. /n
     seq=seq.strip()
+#strings in list that begin with ">"
     if seq.startswith(">"):
+#within those will look for pattern of ">" followed by a number \d+ and ending with a backslash /. Search pattern is called gid
         gid = re.compile(r"(>)(\d+)(/)")
+#search string for pattern in gid
         match= gid.search(seq)
+#if match present
         if match:
+#grab the id 
             id = match.group(2)
+#use the id as the key for sequence in diction input_seqs
             input_seqs[id]=[]
+#if no '>' then add to a list of seqs that belong to the id that superceded it.
     else:
         input_seqs[id].append(seq)
 
-#print input_seqs
-
+#make a file with the same name as the UID input file but with a .fasta file extenstion.
 fasta = open('%s.fasta' %(input_s[0]),'w')
+#Look through each key in the GID_list.
 for seq in GID_list:
+#if gid in dictionary 
     if input_seqs[seq]:
+#write out the id and print joined seqs into new fasta file.
         fasta.write('>%s\n%s\n' %(seq, ''.join(input_seqs[seq])))
-    
+#align fasta file with clustal omega, outputs a clustal alignment    
 phylo_tools.ClustalO(input_s[0])
-
+#build hmm with clustal alignment using hmmbuild
 phylo_tools.hmmbuild('%s_clustalo' %(input_s[0]))
