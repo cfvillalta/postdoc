e4ea0a14a0fde6ad8d85a5f359e7a118c7918c33
Chris Villalta <cfvillalta@gmail.com>
2014-11-18 10:01:43 -0800

    adding comments

--- UID_in_HMM_out_domain.py
+++ UID_in_HMM_out_domain.py
@@ -3,20 +3,29 @@
 import phylo_tools
 import sys
 import re
-#list of UID
+
+#list of UIDs, like a list of UIDs I had picked from java tree view
 input = sys.argv[1]
-input_2 = sys.argv[2]
 #file with fasta seqs
+input_2 = sys.argv[2]
+
+#split file name at '.' to use name later
 input_s = input.split(".")
 
+#open list of UIDs
 input_open =open(input, 'rU')
 
+#read each line of IDs
 GIDs= input_open.readlines()
 
+#create list I will put IDs into.
 GID_list = []
 
+#for loop goes through GIDs list from text file.
 for GID in GIDs:
+#strip /n from each GID
     GID=GID.strip()
+#split each GID by '/' because the GID is a number id, /, and coordinates of domain(in this case tyrosinase domains.)
     GID_s=GID.split("/")
 #    print GID_s[0]
     GID_list.append(GID_s[0])
