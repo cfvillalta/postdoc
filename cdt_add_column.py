45c4b4cefed0dd60a930d0f41c82687f9c9d7e8c
Chris VIllalta <cfvillalta@gmail.com>
2014-11-19 12:55:41 -0800

    added comments to script and cleaned up

--- cdt_add_column.py
+++ cdt_add_column.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+#this script takes in a CDT file used to visiaulize data in java treeview. In my case usually a phylogenetic tree. The script takes in the CDT and adds a new column with taxonomic data e.g. species name from NCBI nr database. The script uses blastdbcmd.
 #will input cdt file
 #extract GID
 #search for GID with BLASTDBCMD
@@ -7,58 +7,75 @@
 import sys
 from subprocess import Popen, PIPE
 
-
+#input cdt file
 cdt_input = sys.argv[1]
-
+#split the file name and extension at "."
 cdt_input_split = cdt_input.split(".")
-
+#open cdt file
 cdt_in =open(cdt_input, 'r')
 
 ############
 #Pushed everything into a dictory with a list of each type of column from the cdt. Should be no dups in the GID
 ############
+#read lines into list from the cdt file.
 cdt_original = cdt_in.readlines()
-#print cdt_original
+#make string called header
 header = ''
+#make string called eweight
 eweight = ''
+#make dictionary called cdt_dict
 cdt_dict = {}
+#make an num 0 to be used to count.
 num=0
+#loop through list of cdt_original
 for line in cdt_original:
-    #print line
+    #strip whitespace. 
     line = line.strip()
+    #if line starts with GID
     if line.startswith('GID'):
+        #add line to header
         header = line
+        #split the header by \t
         header_split = header.split("\t")
+        #insert the TAXID into list at the 5th position
         header_split.insert(5, "TAXID")
+        #join header.split and overwrite old header.
         header = "\t".join(header_split)
-        #print header
+    #if line begins with EWEIGHT
     elif line.startswith('EWEIGHT'):
+        #copy line into eweight.
         eweight = line
+        #split ewight by \t
         eweight_split = eweight.split("\t")
+        #insert a blank space as a \t
         eweight_split.insert(5,"\t")
+        join all eweight by \t
         eweight = "\t".join(eweight_split)
-        #print eweight
+    #if line is non of the above
     else:
+        #add 1 to counter.
         num = num+1
+        #split line by \t.
         line_split = line.split('\t')
-        #        get_GID = line_split[0].split('/')
-        GID = line_split[0]
-        # print GID
+        #split data is input into cdt_dict with the number of the iteration as the key.
         cdt_dict[num] = line_split
-
-        #print cdt_dict
-
 ###################
 #Use Blastdbcmd to get all the tax_ids from nr and add that to a new column in array
 #it will be between GWEIGHT and dummy. Will call TAXID.
 ###################
-
+#loop through cdt_dict.
 for GID in cdt_dict:
+    #from list in cdt_dict grab the string with the GID inside and then split the GID from the sequence infor after the backslash.
     GID_split = cdt_dict[GID][0].split('/')
+    #use Popen to run blastdbcmd as a subprocess. blastdbcmd will run and find the taxonomic information of specific GID.
     p = Popen(['blastdbcmd', '-db', 'nr', '-dbtype', 'prot', '-entry', GID_split[0], '-target_only','-outfmt', '%t'],stdout = PIPE)
+    #extract the output from blastcmd
     stdout = p.stdout.read()
+    #wait until subprocess is done until the script continues forward.
     p.communicate()
+    #if there is a stdout meaning that information for the GID was present in NR
     if stdout:
+        #strip and replace all the charctaers with either whitespace or '_' 
         ID = stdout.strip()
         ID = ID.replace(' ', '_')  
         ID = ID.replace('[', '')
@@ -76,32 +93,30 @@ for GID in cdt_dict:
         ID = ID.replace('__', '_')
         ID = ID.replace('__', '_')
         ID = ID.replace('__', '_')
-        #print ID
-       
+        #insert ID into the fifth position of the list for the current GID within the dict
         cdt_dict[GID].insert(5, ID)
+    #if no output from the subprocess found
     else:  
+        #just insert the GID into the fifth positon of the list for the GID we are accessing from the dict.
         cdt_dict[GID].insert(5, GID_split[0])
-        
-
-#print cdt_dict
 ####################
 #join everything together
 ###################
-
-#print
+#open new cdt file and use the same file name with the addition of _mod.cdt
 cdt_mod = open("%s_mod.cdt" %(cdt_input_split[0]),'w')
-
+#write modified header to modified cdt.
 cdt_mod.write('%s\t\n' %(header))
+#write modified eweight line to modified cdt.
 cdt_mod.write('%s\t\n' %(eweight))
+#loop through cdt_dict keys in numerical order 
 for row in sorted(cdt_dict.keys()):
-    #    print row
-    #print cdt_dict[row]
+    #write each GID and its information onto new modified cdt file.
     cdt_mod.write('%s\t\n' %("\t".join(cdt_dict[row])))
 
 
 
 
-
+#close new modified file. script done.
 cdt_mod.close()
 
     
