c562439b37f92595f6ca37c6bb58c05c5c3cd59f
Chris VIllalta <cfvillalta@gmail.com>
2014-11-19 11:03:33 -0800

    added comments

--- abi_seq_tools.py
+++ abi_seq_tools.py
@@ -1,5 +1,8 @@
 #! /usr/bin/python
+#module with classes to help in analyzing abi seqs.
 
+#class  is used to trim abi seqs of degenerate low quality sequence  using SeqIO.AbiIterator.
+#input a directory into class full of abi seqs.
 def trim(input_directory_here):
 #will be a script where I input a batch of abi files and they are trimmed and out put as sequence files.x
 
@@ -7,28 +10,23 @@ def trim(input_directory_here):
     import os
 
     from Bio import SeqIO
-
+#call directory seq_dir
     seq_dir = input_directory_here
-
+#make a dictionary called seqs.
     seqs = {}
-
+#use os.listdir to make seq_dir into a list of files in dir and loop through it.
     for file in os.listdir(seq_dir):
+#if file ends with .ab1
         if file.endswith(".ab1"):
+#put together the name of the file path in order to open the actual file in the folder.
             file_path = "%s%s" %(seq_dir,file)
-        
+#open .ab1 file from above
             handle = open(file_path, 'rb')
 
 
-
-            for record in SeqIO.AbiIO.AbiIterator(handle, trim = True):
-                #print record.id
-                #print 
-                #print record
-                
+#go through abi and trim
+            for record in SeqIO.AbiIO.AbiIterator(handle, trim = True):             
+#add seq id as key and sequence as value.
                 seqs[record.id] = record
-    
-            # output_handle = open("test.fasta", "w")
-            # SeqIO.write(record, output_handle, "fasta")
-            #output_handle.close()
-
+#return dictionary.
     return(seqs)
