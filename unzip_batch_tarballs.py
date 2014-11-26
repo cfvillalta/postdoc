d9198481ac8bf0ec03320009c088b51e27d34b4f
Chris VIllalta <cfvillalta@gmail.com>
2014-11-26 15:25:44 -0800

    done commenting

--- unzip_batch_tarballs.py
+++ unzip_batch_tarballs.py
@@ -1,18 +1,22 @@
 #! /usr/bin/python/
-
+#program opens multiple tar.gz files in the same directory. Made the script to open ncbi nr database tar files to build nr locally.
 from subprocess import Popen
 import os
 import sys
-
+#name the program I am going to use
 program = 'tar'
-
+#get file path of folder where all the tar files are.
 folder_tars = sys.argv[1]
+#print the folder name so I cam make sure I di it correctly
 print folder_tars
-
+#make a list of files in the directory
 files = os.listdir (folder_tars)
-
+#loop through the list of files
 for file in files:
+    #if the file ends in .tar.gz
     if file.endswith(".tar.gz"):
+        #run subprocess below on file and uncompress into the same folder.
         p = Popen([program,'-zxvf', '%s%s' %(folder_tars,file), '-C', '%s' %(folder_tars)])
+        #wait until file is done compressing and send user message to let them know its done compressing.
         p.communicate('file done being uncompressed')
         
